d9dd3678f1e753a98c406ca858691a1aef5f021d
Laura Pérez Medeiro <laura.pmedeiro@gmail.com>
2023-09-10 17:28:26 +0200

    Update ExploreContainer.tsx to show repo

--- blue-green-pwa/src/components/ExploreContainer.tsx
+++ blue-green-pwa/src/components/ExploreContainer.tsx
@@ -7,7 +7,8 @@ interface ContainerProps {
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
   return (
     <div id="container">
-      <p> This is dev </p>
+      <p> Do you like this project?  </p>
+      <p> Five a start to the <a target="_blank" rel="noopener noreferrer" href=" https://github.com/laurapmedeiro/TFM_DevOps.git"> repository </a></p>
     </div>
   );
 };
