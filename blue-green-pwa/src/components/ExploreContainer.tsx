6f07221cff38496f0760e65e2ca5f47ccc18395a
Laura Pérez Medeiro <laura.pmedeiro@gmail.com>
2023-10-21 20:07:16 +0200

    change welcome message

--- blue-green-pwa/src/components/ExploreContainer.tsx
+++ blue-green-pwa/src/components/ExploreContainer.tsx
@@ -9,6 +9,7 @@ const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
     <div id="container">
       <p> Do you like this project?  </p>
       <p> Five a start to the <a target="_blank" rel="noopener noreferrer" href=" https://github.com/laurapmedeiro/TFM_DevOps.git"> repository </a></p>
+      <p> Or open a ticket with the problem. </p>
     </div>
   );
 };
